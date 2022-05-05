fb348beaf9d04988ec9e151510595cd417256e76
Tatiana Kazakova <test.kazakova@gmail.com>
2022-05-05 22:35:07 +0300

    added system properties tests

--- src/test/java/tests/citilink/SearchCardTest.java
+++ src/test/java/tests/citilink/SearchCardTest.java
@@ -3,6 +3,9 @@ package tests.citilink;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.CollectionCondition.empty;
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
@@ -17,6 +20,12 @@ public class SearchCardTest {
         $("[data-title='Видеокарты']").scrollIntoView(false);
         $("[data-title='Видеокарты']").click();
         $(".Subcategory__header").shouldHave(text("Видеокарты"));
+        $("[title = 'Видеокарта Palit NVIDIA  GeForce RTX 3060," +
+                "  PA-RTX3060 DUAL 12G,  12ГБ, GDDR6, LHR,  Ret [ne63060019k9-190ad]']").shouldHave(text("Видеокарта"));
+        $("[data-index=1 target=_self]").shouldHave(text("Видеокарта"));
+
+
+
 
 
     }
